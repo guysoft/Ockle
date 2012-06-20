994117305180765af824338397358b1785f657c8
guysoft <guysoft@gmail.com>
2012-06-20 13:24:23 +0300

    Testing graphic display of graph

---
+++ src/plugins/GaraphTester.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+"""  Ockle PDU and servers manager
+Test plugin to check graph capabilities
+
+Created on May 16, 2012
+
+@author: Guy Sheffer <guy.sheffer at mail.huji.ac.il>
+"""
+import os.path,sys
+p = os.path.join(os.path.dirname(os.path.realpath(__file__)),'..','common')
+sys.path.insert(0, p)
+import time
+from plugins.ModuleTemplate import ModuleTemplate
+import pygraph.readwrite.dot
+
+class GraphTester(ModuleTemplate):
+    def __init__(self,MainDaemon):
+        ModuleTemplate.__init__(self,MainDaemon)
+        return
+    
+    def run(self):
+        self.debug("\n")
+        print "yay"
+        #print self.mainDaemon.servers.graph
+        print pygraph.readwrite.dot.write(self.mainDaemon.servers.graph,True)
+        
+                
+    
+        return
+
+if __name__ == "__main__":
+    a = GraphTester(None)
