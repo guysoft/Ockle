0b8ea0e2ea2265f973c71e51546cfcf965a30bb3
Guy Sheffer <guysoft@gmail.com>
2012-10-07 04:24:06 +0200

    Add a dummy controller with CPU, and a controller using SSH (untested)

---
+++ src/controllers/ControllerDummyWithCPU.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+""" Ockle PDU and servers manager
+A dummy controller, useful for testing
+
+Created on Oct 05, 2012
+
+@author: Guy Sheffer <guy.sheffer at mail.huji.ac.il>
+"""
+from controllers.ControllerDummy import Dummy
+
+import time
+
+class CPUsage:
+    def __init__(self, interval=0.1, percentage=True):
+        self.interval=interval
+        self.percentage=percentage
+        self.result=self.compute()
+      
+    def get_time(self):
+        stat_file=file("/proc/stat", "r")
+        time_list=stat_file.readline().split(" ")[2:6]
+        stat_file.close()
+        for i in range(len(time_list)):
+            time_list[i]=int(time_list[i])
+        return time_list
+  
+    def delta_time(self):
+        x=self.get_time()
+        time.sleep(self.interval)
+        y=self.get_time()
+        for i in range(len(x)):
+            y[i]-=x[i]
+        return y   
+
+    def compute(self):
+        t=self.delta_time()
+        if self.percentage:
+            result=100-(t[len(t)-1]*100.00/sum(t))
+        else:
+            result=sum(t)
+        return result
+  
+    def __repr__(self):
+        return str(self.result)
+    
+class DummyWithCPU(Dummy):
+    def __init__(self,name,controllerConfigDict={},controllerParams={}):
+        Dummy.__init__(self,name,controllerConfigDict={},controllerParams={})
+        
+        self.cpu = CPUsage()
+        self.data["CPU"]=0
+        return
+
+    def updateData(self):
+        self.data["CPU"] = self.cpu.compute()
+        return self.data
---
+++ src/controllers/SSHController.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+""" Ockle PDU and servers manager
+A dummy controller, useful for testing
+
+Created on Oct 05, 2012
+
+@author: Guy Sheffer <guy.sheffer at mail.huji.ac.il>
+"""
+from controllers.ControllerTemplate import ControllerTemplate
+
+from paramiko import SSHClient
+import json
+
+class SSHController(ControllerTemplate):
+    def __init__(self,name,controllerConfigDict={},controllerParams={}):
+        ControllerTemplate.__init__(self,name,controllerConfigDict={},controllerParams={})
+        
+        self.sshArgs = {"host": controllerParams["host"],
+                        "password" : controllerParams["password"],
+                        "username" : controllerParams["username"],
+                        "key_filename" : controllerParams["key_filename"],
+                        "allow_agent" : json.loads(controllerParams["allow_agent"]),
+                        "look_for_keys" : json.loads(controllerParams["look_for_keys"]),
+                        "compress" : json.loads(controllerParams["compress"])
+                        }
+        try:
+            self.sshArgs["timeout"] = float(controllerParams["timeout"])
+        except ValueError:
+            pass  
+        
+        for arg in self.sshArgs.keys():
+            if self.sshArgs[arg] == "" and arg != "host":
+                self.sshArgs.pop(arg)
+        
+        self.state=False
+        return
+    
+    def updateData(self):
+        pass
+    
+    def setSate(self,state):
+        if state == False:
+            client = SSHClient()
+            client.load_system_host_keys()
+            client.connect(** self.sshArgs)
+            #stdin, stdout, stderr = client.exec_command('ls -l')
+            client.exec_command('shutdown now')
+            client.close()
+        return
